9e48a8f8ce73fd542be07d314e271b4a104215bc
Yadenian <dan-yan99@mail.ru>
2026-01-28 11:16:48 +0300

    Refactor shared components and remove legacy App entry. Also added new icon to the project

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,5 @@
-export const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center min-h-[400px]">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-600"></div>
-    </div>
-  );
-};
+export const LoadingSpinner = () => (
+  <div className="flex justify-center items-center min-h-[400px]">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-600"></div>
+  </div>
+);
